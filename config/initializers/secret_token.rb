9410a4e25e480e26e67949147b1a672fb6fa14db
Yutaka Imoto <yutaka.imoto@dena.com>
2014-11-15 16:29:40 +0900

    rails new Kakarot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kakarot::Application.config.secret_key_base = '6ab592bf2600e46c237918a45f71bdc50d4c7f621d698c2506a1e5452d6821bd4bfb7543cf41424bc00f54fa5cc34d7a2123e4860dcb160dab27d1150c35de77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kakarot::Application.config.session_store :cookie_store, key: '_Kakarot_session'
