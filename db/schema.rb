c3b9b8f759f1814a16db2fb0e3c073370de97ebc
Yutaka Imoto <yutaka.imoto@dena.com>
2014-11-16 11:33:11 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116013537) do
+ActiveRecord::Schema.define(version: 20141116015629) do
 
   create_table "arrangements", force: true do |t|
     t.datetime "created_at"
