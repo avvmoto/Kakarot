7e3ef5ca1a6e2bbbffe277f9ada46c71527b0150
Yutaka Imoto <yutaka.imoto@dena.com>
2014-11-16 15:51:42 +0900

    Add asset helper

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ActiveRecord::Base
   belongs_to :arrangement
-  def image_path
-    
+  def image_asset
+    return nil if image_uri.nil?
+    'recipe/' + image_uri.split('/')[-1]
   end
 end
