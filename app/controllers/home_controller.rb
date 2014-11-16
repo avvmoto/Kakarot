b84202b033d3e1bb0818bbb0a8ff6ed083d32a34
Yutaka Imoto <yutaka.imoto@dena.com>
2014-11-16 13:32:37 +0900

    Add main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @tweets = Tweet.order('id DESC').limit(20)
+    @target_tweet = Tweet.find(1)
+    @colors = @target_tweet.mood.arrangement.color
   end
 end
--- app/helpers/colors_helper.rb
+++ app/helpers/colors_helper.rb
@@ -1,2 +1,10 @@
 module ColorsHelper
+  def format_code(color)
+    return nil if color.code.nil?
+    color.code.gsub(/^#/, '').downcase
+  end
+
+  def format_weight(color)
+    sprintf("%02d", color.weight)
+  end
 end
