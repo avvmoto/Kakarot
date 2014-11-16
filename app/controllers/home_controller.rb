a5f668d1dd2d2aa1589fe4f78ee1f99cfa5364e7
Taiyo Akashi <onibakidd@gmail.com>
2014-11-16 14:29:21 +0900

    fix bug with view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @tweets = Tweet.order('id DESC').limit(20)
-    @target_tweet = Tweet.find(1)
-    @colors = @target_tweet.mood.arrangement.color
+    unless current_user
+      redirect_to user_omniauth_authorize_path(:twitter)
+    else
+      @tweets = Tweet.where.not(mood_id: nil).limit(3)
+      @recipe = Recipe.first #TODO: 後でムードからの処理結果を取ってくるようにする
+      @target_tweet = @tweets.first
+      @colors = []
+      # @colors = @target_tweet.mood.arrangement.color
+    end
   end
 end
--- app/controllers/single_pages_controller.rb
+++ app/controllers/single_pages_controller.rb
@@ -3,7 +3,6 @@ class SinglePagesController < ApplicationController
     unless current_user
       redirect_to user_omniauth_authorize_path(:twitter)
     else
-      @tweets = Tweet.where.not(mood_id: nil).limit(3)
     end
   end
 end
