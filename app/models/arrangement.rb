daf11c22ba2c79f82677fd0216b1697be05e092c
Yutaka Imoto <yutaka.imoto@dena.com>
2014-11-16 15:24:35 +0900

    Fix model

--- app/models/arrangement.rb
+++ app/models/arrangement.rb
@@ -1,5 +1,6 @@
 class Arrangement < ActiveRecord::Base
   belongs_to :mood
-  has_and_belongs_to_many :color
+  has_many :arrangements_color
+  has_many :color, through: :arrangements_color
   has_many :recipe
 end
--- app/models/arrangements_color.rb
+++ app/models/arrangements_color.rb
@@ -1,2 +1,6 @@
 class ArrangementsColor < ActiveRecord::Base
+  belongs_to :color
+  belongs_to :arrangement
+
+  delegate :weight, to: :color
 end
